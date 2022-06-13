fa1151f90035b5509394392beb1f115501bf120f
Connie Vi <connievi@fb.com>
2022-06-13 15:43:34 -0700

    implemented logout feature

--- app/src/main/java/com/example/instagram/ParseApplication.java
+++ app/src/main/java/com/example/instagram/ParseApplication.java
@@ -1,2 +1,22 @@
-package com.example.instagram;public class ParseApplication {
+package com.example.instagram;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class ParseApplication extends Application {
+
+    // Initializes Parse SDK as soon as the application is created
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Register parse models
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("n6LMhbehP5JevoMSeAYifUVcM2fCJqJnrdunv6F5")
+                .clientKey("WdNUYc74eSQ1E19xcK9XowQEoS14koR4LTkH5Awz")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
 }
